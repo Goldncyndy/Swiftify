54005f9f4fd2979a9fe1722800c6af1a97d2cb25
Ezeh Cynthia Ifeoma <goldncyndy@gmail.com>
2024-07-30 14:21:37 +0100

    Implement Onboarding and signup UI Conversion

---
+++ Swiftify/Swiftify/Controllers/ChatControllers/ChatViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatViewController.swift
+//  Swiftify
+//
+//  Created by Cynthia D'Phoenix on 7/30/24.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ Swiftify/Swiftify/Controllers/Signin/SigninViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SigninViewController.swift
+//  Swiftify
+//
+//  Created by Cynthia D'Phoenix on 7/30/24.
+//
+
+import UIKit
+
+
+class SignInViewController: UIViewController {
+
+    
+}
